215ee12e7a55e885e87d1f804971abe03846bdee
ghufranspa <aleem.ghufran@spaplc.com>
2023-10-27 15:47:11 +0530

    remove all unnecessary files and folders and modified app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GraphqlModule } from './graphql/graphql.module';
-import { SampleResolver } from './sample/sample.resolver';
+
 
 @Module({
-  imports: [GraphqlModule],
+
   controllers: [AppController],
-  providers: [AppService, SampleResolver],
+  providers: [AppService],
 })
 export class AppModule {}
